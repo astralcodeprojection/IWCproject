6fd8eb97009882d521a0bd320f2cac9e15596172
bayareadesignco <moller76551@gotoltc.edu>
2022-03-11 02:09:26 -0600

    added carousel

--- index.php
+++ index.php
@@ -6,12 +6,15 @@
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>Sustain Jewelry Co.</title>
     <link rel="stylesheet" href="assets/css/style.css">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
 </head>
 
 <body class="">
     <?php include("nav.html");?>
 
-    <section id="home" class="mainPage">
+    <section id="home" class="homepage">
         <div class="container">
             <div class="row text-center">
 
@@ -20,29 +23,40 @@
             </div>
             <br><br>
 
-            <!-- START THE DESCRIPTION CONTENT  -->
-            
+           
             <div class="row">
-                <div class="col-md-6">
-                    <div class="bg-white p-4 text-start">
-                        <p class="fw-light">
-                            Lorem ipsum dolor sit amet consectetur architecto magni,
-                            dicta maxime laborum temporibus dolorem esse doloremque illo quas nisi enim molestias.
-                            Tempore ducimus molestiae in dolore enim.
-                        </p>
+                <div class="col-md-1"></div>
+                <div class="col-md-10">
+                    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        </div>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="img/Jewelry_Wood_planks_542588_1920x1080.jpg" class="d-block w-100" alt="wood jewelry">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="img/two.jpg" class="d-block w-100" alt="filler">
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
                     </div>
                 </div>
-                <div class="col-md-6">
-                    <div class="bg-white p-4 text-start">
-                        <p class="fw-light">
-                            a 6 column text post a 6 column text post a 6 column text post a 6 column text post a 6
-                            column text post a 6 column text post a 6 column text post a 6 column text post
-                        </p>
-                    </div>
-                </div>
+                <div class="col-md-1"></div>
             </div>
+            
         </div>
     </section>
+    <br><br><br>
+    
     
 
 </body>
