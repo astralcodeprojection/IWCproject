874ce6ef686849c62e43b0155ec6df271ee02ee5
bayareadesignco <moller76551@gotoltc.edu>
2022-03-10 18:50:54 -0600

    Working with index page - changed to php to add php functionality to page

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Sustain Jewelry Co.</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body class="">
+    <?php include("nav.html");?>
+
+    <section id="home" class="mainPage">
+        <div class="container">
+            <div class="row text-center">
+
+                <h1 class="display-3 fw-bold">Hola</h1>
+                <div class="heading-line mb-1"></div>
+            </div>
+            <br><br>
+
+            <!-- START THE DESCRIPTION CONTENT  -->
+            
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="bg-white p-4 text-start">
+                        <p class="fw-light">
+                            Lorem ipsum dolor sit amet consectetur architecto magni,
+                            dicta maxime laborum temporibus dolorem esse doloremque illo quas nisi enim molestias.
+                            Tempore ducimus molestiae in dolore enim.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="bg-white p-4 text-start">
+                        <p class="fw-light">
+                            a 6 column text post a 6 column text post a 6 column text post a 6 column text post a 6
+                            column text post a 6 column text post a 6 column text post a 6 column text post
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php include("navfooter.php");?>
+
+</body>
+
+
+</html>
