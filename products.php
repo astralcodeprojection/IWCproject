92ef98e39ae5fd9f9ccc5cc557ffe1748afe98eb
bayareadesignco <moller76551@gotoltc.edu>
2022-03-20 22:13:31 -0500

    added base products page

---
+++ products.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Sustain Jewelry Co.</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</head>
+
+<body class="">
+    <?php include("nav.html");?>
+
+    
+    
+
+</body>
+<?php include("navfooter.php");?>
+
+</html>
