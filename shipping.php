1fe6a5d5c5e3a5104e06ff6a83c9deb44bdb6beb
Banks Meacham <meachambanks@gmail.com>
2022-04-20 12:54:06 -0500

    added confirmOrder functionality as well as shipping

--- shipping.php
+++ shipping.php
@@ -28,8 +28,149 @@ session_start();
     <?php include("warningBanner.php");?>
     <?php include("nav.html");?>
 
+    <?php
+        if(isset($_POST["items"])){
+            //items here
+            $items = $_POST["items"];
+            $userId = $_POST["userId"];
+            $totalPrice = $_POST["total"];
+        } else{
+            //redirect cart
+            header('Location:cart.php');
+        }
+
+        if($_SESSION["logged_in"] != "true"){
+            //no data for customer to grab
+        }else {
+            $customerSQL = "select * from users WHERE userId = :userId";
+        
+            $customerStatement = $db->prepare($customerSQL);
+            $customerStatement -> bindValue(':userId' , $userId);
+            
+        
+            if($customerStatement->execute()){
+                $customers = $customerStatement->fetchAll();
+                $customerStatement->closeCursor();
+            }else{
+                $error1= "Error finding customer.";
+            }
+            $thisCustomer;
+            foreach($customers as $customer){
+                $thisCustomer = $customer;
+            }
+        }
+     ?>
     
     
+    <div class="container">
+        <div class="row text-center">
+            <h2 class="display-3 fw-bold">Shipping Method</h2>
+            <div class="heading-line mb-1"></div>
+        </div>
+        <form action="payment.php" method="POST">
+            <div class="row">
+                <div class="col">
+                    <input name="fname" type="text" class="form-control" placeholder="First name" aria-label="First name" value="<?php echo $thisCustomer["firstName"]?>">
+                </div>
+                <div class="col">
+                    <input name="lastname" type="text" class="form-control" placeholder="Last name" aria-label = "Last name" value="<?php echo $thisCustomer["lastName"]?>">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <input name="address" type="text" class="form-control" placeholder="555 Cherry St." aria-label="Address" value="<?php echo $thisCustomer["address"]?>">
+                </div>
+                <div class="col">
+                    <input name="city" type="text" class="form-control" placeholder="City" aria-label="City" value="<?php echo $thisCustomer["city"]?>">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <input name="zip" type="text" class="form-control" placeholder="XXXXX" aria-label="Zip Code" value="<?php echo $thisCustomer["zip"]?>">
+                </div>
+                <div class="col">
+                    <select name="state" class="form-select" placeholder="State" aria-label="State" value="<?php echo $thisCustomer["state"]?>">
+                        <option value="AL">Alabama</option>
+                        <option value="AK">Alaska</option>
+                        <option value="AZ">Arizona</option>
+                        <option value="AR">Arkansas</option>
+                        <option value="CA">California</option>
+                        <option value="CO">Colorado</option>
+                        <option value="CT">Connecticut</option>
+                        <option value="DE">Delaware</option>
+                        <option value="FL">Florida</option>
+                        <option value="GA">Georgia</option>
+                        <option value="HI">Hawaii</option>
+                        <option value="ID">Idaho</option>
+                        <option value="IL">Illinois</option>
+                        <option value="IN">Indiana</option>
+                        <option value="IA">Iowa</option>
+                        <option value="KS">Kansas</option>
+                        <option value="KY">Kentucky</option>
+                        <option value="LA">Louisiana</option>
+                        <option value="ME">Maine</option>
+                        <option value="MD">Maryland</option>
+                        <option value="MA">Massachusetts</option>
+                        <option value="MI">Michigan</option>
+                        <option value="MN">Minnesota</option>
+                        <option value="MS">Mississippi</option>
+                        <option value="MO">Missouri</option>
+                        <option value="MT">Montana</option>
+                        <option value="NE">Nebraska</option>
+                        <option value="NV">Nevada</option>
+                        <option value="NH">New Hampshire</option>
+                        <option value="NJ">New Jersey</option>
+                        <option value="NM">New Mexico</option>
+                        <option value="NY">New York</option>
+                        <option value="NC">North Carolina</option>
+                        <option value="ND">North Dakota</option>
+                        <option value="OH">Ohio</option>
+                        <option value="OK">Oklahoma</option>
+                        <option value="OR">Oregon</option>
+                        <option value="PA">Pennsylvania</option>
+                        <option value="RI">Rhode Island</option>
+                        <option value="SC">South Carolina</option>
+                        <option value="SD">South Dakota</option>
+                        <option value="TN">Tennessee</option>
+                        <option value="TX">Texas</option>
+                        <option value="UT">Utah</option>
+                        <option value="VT">Vermont</option>
+                        <option value="VA">Virginia</option>
+                        <option value="WA">Washington</option>
+                        <option value="WV">West Virginia</option>
+                        <option value="WI">Wisconsin</option>
+                        <option value="WY">Wyoming</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+                    <label class="form-check-label" for="exampleRadios1">
+                        Standard Shipping
+                    </label>
+                    </div>
+                    <div class="form-check">
+                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+                    <label class="form-check-label" for="exampleRadios2">
+                        Express Shipping
+                    </label>
+                    </div>
+                </div>
+            </div>
+            
+            <input type="hidden" name="userId" value="<?php echo $userId?>">
+            <input type="hidden" name="items" value="<?php echo $items?>">
+            <input type="hidden" name="total" value="<?php echo $totalPrice?>">
+        </form>
+            <p style="text-align: center; font-size: 1.1rem; font-weight: bold;">Total: $<?php echo $totalPrice?></p>
+            <div style="text-align: center; margin: 2%;">
+                <button style="padding: 1%; background-color: rgb(59, 110, 85); color: #FFFFFF; border: 0px; border-radius: 15px; font-size: 1.1rem;" type="submit">Proceed to Payment</button>
+        </div>
+            
+        </div>
+    </div>
     
 
 </body>
